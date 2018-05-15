9faa24683739b997e2aea211a65892cd58e92cf8
zhengpenghui <zhengpenghui@qingting.fm>
2018-05-15 18:57:14 +0800

    feat(sites): Add `sites` sub module

--- mirage/config.js
+++ mirage/config.js
@@ -14,7 +14,7 @@ import mockUsers from './config/users';
 // import {versionMismatchResponse} from 'utils';
 
 export default function () {
-    this.urlPrefix = 'http://localhost:2368'; // make this `http://localhost:8080`, for example, if your API is on a different server
+    this.urlPrefix = 'http://localhost:8080'; // make this `http://localhost:8080`, for example, if your API is on a different server
     this.namespace = '/ghost/api/v0.1'; // make this `api`, for example, if your API is namespaced
     this.timing = 400; // delay for each request, automatically set to 0 during testing
 
@@ -34,7 +34,7 @@ export default function () {
 // Mock all endpoints here as there is no real API during testing
 export function testConfig() {
     this.passthrough('/write-coverage'); // For code coverage
-    this.urlPrefix = 'http://localhost:2368'; // make this `http://localhost:8080`, for example, if your API is on a different server
+    this.urlPrefix = 'http://localhost:8080'; // make this `http://localhost:8080`, for example, if your API is on a different server
     this.namespace = '/ghost/api/v0.1'; // make this `api`, for example, if your API is namespaced
     // this.timing = 400;      // delay for each request, automatically set to 0 during testing
     // this.logging = true;
